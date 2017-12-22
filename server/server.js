d34c66364b3e2cf6a822a60b19bfaa10c478aee1
m8601 <robert.szymanski08@gmail.com>
2017-12-22 11:59:18 +0100

    Fixed app.listen

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
