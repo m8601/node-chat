8a4e9643014de77cdbf9ecf892245bcfaa476298
m8601 <robert.szymanski08@gmail.com>
2017-12-26 15:39:17 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to the server');
 
-  socket.emit('createMessage', {
-    from: 'Adam',
-    text: 'Yes, you sucks big dicks, big time'
-  });
-
 });
 
 socket.on('newMessage', function(message) {
