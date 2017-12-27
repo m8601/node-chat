0cc26b302eef73b8d1d9682fc4783557598d2925
m8601 <robert.szymanski08@gmail.com>
2017-12-27 16:31:36 +0100

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,13 +29,14 @@ socket.on('newLocationMessage', function(message) {
 });
 
 $('#message-form').on('submit', function(e) {
+  var messageTextBox =  $('[name=message]');
   e.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 });
 
@@ -45,12 +46,16 @@ locationButton.on('click', function() {
     return alert('Geolocation not supportedby your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
