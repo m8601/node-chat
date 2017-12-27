83d9068d5f5559fdcc890fcb435ef3fcc058ca01
m8601 <robert.szymanski08@gmail.com>
2017-12-27 14:58:27 +0100

    Add form for messages andshow incoming messages in browsers

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,23 @@ socket.on('connect', function() {
 socket.on('newMessage', function(message) {
   console.log('New Message from ' + message.from + '.');
   console.log(message);
+
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`)
+
+  $('#messages').append(li);
 });
 socket.on('disconnect', function() {
   console.log('Disconnected from server')
+});
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
