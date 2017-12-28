a2676a6d25902a9cdbe9939bbf427562a1b5900c
m8601 <robert.szymanski08@gmail.com>
2017-12-28 13:30:06 +0100

    Add mustachejs for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -5,29 +5,49 @@ socket.on('connect', function() {
 
 });
 
-socket.on('newMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
-  console.log('New Message from ' + message.from + '.');
-  console.log(message);
-
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
-
-  $('#messages').append(li);
-});
 socket.on('disconnect', function() {
   console.log('Disconnected from server')
 });
 
+socket.on('newMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a')
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(html);
+
+  // console.log('New Message from ' + message.from + '.');
+  // console.log(message);
+  //
+  // var li = $('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`)
+  //
+  // $('#messages').append(li);
+});
+
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  $('#messages').append(html);
+
+  // var li = $('<li></li>');
+  // var a = $('<a target="_blank">My current location</a>');
+  //
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href', message.url);
+  // li.append(a);
+  // $('#messages').append(li);
 });
 
 $('#message-form').on('submit', function(e) {
